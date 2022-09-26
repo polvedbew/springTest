1c59c56810ad1dd73c3f71872f67f5832bb926ab
Athira John <athira@debian-BULLSEYE-live-builder-AMD64>
2022-09-26 16:10:24 +0530

    test

--- src/main/java/com/norden/secnor/Main.java
+++ src/main/java/com/norden/secnor/Main.java
@@ -13,9 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         System.out.println("starting server");
-
-
-
         SpringApplication.run(Main.class, args);
+
     }
+
 }
